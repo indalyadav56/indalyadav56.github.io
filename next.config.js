9df788b09406b350a1e1677fe35f986bf78dc5e8
Indal Yadav <indal@paydoh.money>
2025-03-14 12:49:40 +0530

    wip

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   images: {
     unoptimized: true
   },
+  // Add basePath for GitHub Pages deployment
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
+  // Disable trailing slashes
+  trailingSlash: false,
 };
 
 module.exports = nextConfig;
